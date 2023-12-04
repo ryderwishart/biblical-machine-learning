a10bec4a49db54f1998853487e70da528c453c35
Nathan <50530898+nkrbrock@users.noreply.github.com>
2023-12-04 10:54:27 -0600

    cleaned up design and added sticky header

--- alignments/evaluation/table_script.js
+++ alignments/evaluation/table_script.js
@@ -91,7 +91,7 @@ csvData.forEach((rowData, rowIndex) => {
           if (selectedButton) {
             selectedButton.style.backgroundColor = 'green'; // Deselect the currently selected button
           }
-          button.style.backgroundColor = 'blue'; // Select the new button
+          button.style.backgroundColor = '#39A7FF'; // Select the new button
           selectedButton = button; // Update the currently selected button
         });
         buttonContainer.appendChild(button);
@@ -245,3 +245,20 @@ for (let i = 0; i < rows.length; i++) {
 const rootElement = document.getElementById('root');
 rootElement.appendChild(App());
 
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {stickyHeader()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyHeader() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
